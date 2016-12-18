877b8b174d9c0011fe68df667b1b8dc727cb5cdb
sam g <sghaneka@yahoo.com>
2016-12-18 12:24:56 -0500

    getting the dialogs up

--- app/src/main/java/criminalintent/android/bignerdranch/packagecom/criminalintent/DatePickerFragment.java
+++ app/src/main/java/criminalintent/android/bignerdranch/packagecom/criminalintent/DatePickerFragment.java
@@ -5,6 +5,8 @@ import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
 
 /**
  * Created by samgh on 12/13/2016.
@@ -12,9 +14,15 @@ import android.support.v7.app.AlertDialog;
 
 public class DatePickerFragment extends DialogFragment {
 
+
+
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState){
+        View v = LayoutInflater.from(getActivity())
+                .inflate(R.layout.dialog_date, null);
+
         return new AlertDialog.Builder(getActivity())
+              .setView(v)
                 .setTitle(R.string.date_picker_title)
                 .setPositiveButton(android.R.string.ok, null)
                 .create();
